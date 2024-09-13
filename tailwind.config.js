1b97d8e9d93ebeec1342d4375fc3399f34023624
mominulislamsharon <ilsamikchannel328250@gmail.com>
2024-09-14 03:19:10 +0600

    added navbar

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      // colors: {
+      //   // primary: "##437fc1",
+      // },
+    },
+  },
+  plugins: [],
+}
